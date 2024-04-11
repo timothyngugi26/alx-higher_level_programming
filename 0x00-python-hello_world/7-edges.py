7548eefcfd727969ede617ab2ce58cc03da77bb5
timothyngugi26 <timothyngugi26@gmail.com>
2024-04-10 23:46:23 -0700

    0x00-python-hello_world

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"word: {word[0:2]}")
-print(f"word: {word[-2:]}")
-print(f"word: {word[1:-1]}")
+print(f"First 3 letters: {word[0:2]}")
+print(f"Last two letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
