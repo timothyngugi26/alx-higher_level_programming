d2d6976147c3f4f7f7aad6610ee082330e2e7348
timothyngugi26 <timothyngugi26@gmail.com>
2024-06-18 00:48:55 -0700

    JavaSCript Warmup 1.

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-/*print the three lines*/
-MyVar = "C is fun\nPython is cool\nJavaScript is amazing\n";
-console.log(MyVar);
+/* print the three lines */
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing\n';
+console.log(myVar);
